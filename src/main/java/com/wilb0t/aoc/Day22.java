010408bd7bf38cb3d6cd1e98db0d4a540657fbad
Reid Wilbur <reid.wilbur@gmail.com>
2024-12-22 14:39:15 -0600

    Day22 sln 2

--- src/main/java/com/wilb0t/aoc/Day22.java
+++ src/main/java/com/wilb0t/aoc/Day22.java
@@ -1,5 +1,11 @@
 package com.wilb0t.aoc;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
 public class Day22 {
 
   public static long getSecretNumSum(int[] input) {
@@ -12,6 +18,54 @@ public class Day22 {
       sum += next;
     }
     return sum;
+  }
+
+  public static long getMaxBananas(int[] input) {
+    var monkeys = new ArrayList<Monkey>();
+    var seqs = new HashSet<List<Integer>>();
+    for (var num : input) {
+      var monkey = Monkey.of(num, 2000);
+      monkeys.add(monkey);
+      seqs.addAll(monkey.seqVals.keySet());
+    }
+
+    var maxBananas = 0;
+    var maxSeq = List.<Integer>of();
+    for (var seq : seqs) {
+      var bananas = 0;
+      for (var monkey : monkeys) {
+        bananas += monkey.seqVals.getOrDefault(seq, 0);
+      }
+      if (bananas > maxBananas) {
+        maxBananas = bananas;
+        maxSeq = seq;
+      }
+    }
+    return maxBananas;
+  }
+
+  public record Monkey(Map<List<Integer>, Integer> seqVals) {
+    public static Monkey of(int snum, int numDiffs) {
+      var diffs = new ArrayList<Integer>();
+      var nums = new ArrayList<Integer>();
+      long last = snum;
+      for (var i = 0; i < numDiffs; i++) {
+        var next = getNextNum(last);
+        var diff = (next % 10) - (last % 10);
+        diffs.add((int) diff);
+        nums.add((int) (next % 10));
+        last = next;
+      }
+
+      var seqVals = new HashMap<List<Integer>, Integer>(numDiffs - 4);
+      for (var i = 0; i < diffs.size() - 4; i++) {
+        var seq = List.of(diffs.get(i), diffs.get(i + 1), diffs.get(i + 2), diffs.get(i + 3));
+        if (!seqVals.containsKey(seq)) {
+          seqVals.put(seq, nums.get(i + 3));
+        }
+      }
+      return new Monkey(seqVals);
+    }
   }
 
   static long getNextNum(long num) {
--- src/test/java/com/wilb0t/aoc/Day22Test.java
+++ src/test/java/com/wilb0t/aoc/Day22Test.java
@@ -2,11 +2,13 @@ package com.wilb0t.aoc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class Day22Test {
 
   private static final int[] TEST_INPUT = Input.TEST.loadInts();
+  private static final int[] TEST_INPUT2 = new Input("-test2").loadInts();
   private static final int[] PUZZLE_INPUT = Input.PUZZLE.loadInts();
 
   @Test
@@ -20,9 +22,31 @@ public class Day22Test {
   }
 
   @Test
+  public void testGetMaxBananas_testInput() {
+    assertThat(Day22.getMaxBananas(TEST_INPUT2)).isEqualTo(23L);
+  }
+
+  @Test
+  public void testGetMaxBananas_puzzleInput() {
+    assertThat(Day22.getMaxBananas(PUZZLE_INPUT)).isEqualTo(1696L);
+  }
+
+  @Test
   public void testGetNextNum() {
     assertThat(Day22.getNextNum(123)).isEqualTo(15887950);
     assertThat(Day22.getNextNum(15887950)).isEqualTo(16495136);
     assertThat(Day22.getNextNum(16495136)).isEqualTo(527345);
+  }
+
+  @Test
+  public void testMonkey() {
+    var monkey = Day22.Monkey.of(123, 10);
+    assertThat(monkey.seqVals().get(List.of(-3, 6, -1, -1))).isEqualTo(4);
+    assertThat(monkey.seqVals().get(List.of(6, -1, -1, 0))).isEqualTo(4);
+    assertThat(monkey.seqVals().get(List.of(-1, -1, 0, 2))).isEqualTo(6);
+    assertThat(monkey.seqVals().get(List.of(-1, 0, 2, -2))).isEqualTo(4);
+    assertThat(monkey.seqVals().get(List.of(0, 2, -2, 0))).isEqualTo(4);
+    assertThat(monkey.seqVals().get(List.of(2, -2, 0, -2))).isEqualTo(2);
+    assertThat(monkey.seqVals()).hasSize(6);
   }
 }
